fa6bbc30cea8ac7543008822a94047ed48a63647
Lakshmi_ML <42915433+Lakshmi786@users.noreply.github.com>
2024-12-31 06:25:46 +0000

    Model training updated

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationPipeline
+from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -31,6 +32,17 @@ try:
         logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataTransformationPipeline()
         obj.initiate_data_transformation()
+        logger.info(f">>>> stage {STAGE_NAME} completed <<<<<<<\n\n=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer stage"
+
+try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerPipeline()
+        obj.initiate_model_training()
         logger.info(f">>>> stage {STAGE_NAME} completed <<<<<<<\n\n=========x")
 except Exception as e:
     logger.exception(e)
